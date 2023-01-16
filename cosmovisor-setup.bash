e9a054f558981aacd1ff99cd4909861b71da25af
Thomas Bush <bushthomas@hotmail.com>
2023-01-16 13:13:35 +0100

    add shebang

--- cosmovisor-setup.bash
+++ cosmovisor-setup.bash
@@ -1,13 +1,13 @@
+#!/bin/sh
+
 # Create Cosmovisor Folders
 mkdir -p ~/.cantod/cosmovisor/genesis/bin
 mkdir -p ~/.cantod/cosmovisor/upgrades
-
 
 # Load Node Binary into Cosmovisor Folder
 cp /usr/bin/cantod ~/.cantod/cosmovisor/genesis/bin
 
 # Prep upgraded binaries
-
 cd ~/Canto
 git checkout v2.0.0
 make install
@@ -16,7 +16,6 @@ mkdir -p ~/.cantod/cosmovisor/upgrades/v2.0.0/bin
 
 cp ~/go/bin/cantod ~/.cantod/cosmovisor/upgrades/v2.0.0/bin
 
-cd ~/Canto
 git checkout v3.0.0
 make install
 
@@ -24,7 +23,6 @@ mkdir -p ~/.cantod/cosmovisor/upgrades/v3.0.0/bin
 
 cp ~/go/bin/cantod ~/.cantod/cosmovisor/upgrades/v3.0.0/bin
 
-cd ~/Canto
 git checkout v4.0.0
 make install
 
